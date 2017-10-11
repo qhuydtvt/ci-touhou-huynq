ae4f4bd4a5022cd987cafbf7445434dec03ed531
huynq <qhuydtvt@gmail.com>
2017-10-11 19:37:32 +0700

    Added game canvas

---
+++ src/GameCanvas.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by huynq on 10/11/17.
+ */
+public class GameCanvas extends JPanel {
+
+    public GameCanvas() {
+
+    }
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -6,8 +6,14 @@ import java.awt.*;
  */
 public class GameWindow extends JFrame {
 
+    GameCanvas canvas;
+
     public GameWindow() {
         this.setSize(800, 600);
         this.setVisible(true);
+        this.setBackground(Color.black);
+
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
     }
 }
